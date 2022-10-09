3a8f8de4e06a7b57a3419f814d55fbee1d3aaf3e
Roshan John <91770248+RoshanJ20@users.noreply.github.com>
2022-10-09 19:49:03 +0530

    third commit

--- Main.py
+++ Main.py
@@ -1,9 +1,23 @@
 from typing import List
 
-def quick_sort(data, low, high) -> List[int]:
-    # Write code here
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
 
-
+def quickSort(array, low, high):
+    if low < high:
+        pi = partition(array, low, high)
+        quickSort(array, low, pi - 1)
+        quickSort(array, pi + 1, high)
+ 
+ 
+ 
 input_data = input()
 data = []
 for item in input_data.split(', '):
